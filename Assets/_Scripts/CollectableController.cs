08ebfa47eb741f540dc02b1a899322c4ac0515af
PikaKight <tmmarcus73@gmail.com>
2025-04-18 17:49:27 -0400

    added collectables

--- Assets/_Scripts/CollectableController.cs
+++ Assets/_Scripts/CollectableController.cs
@@ -5,12 +5,28 @@ public class CollectableController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag != "Player")
         {
-            int flames = PlayerPrefs.GetInt("Flames", 0);
-
-            PlayerPrefs.SetInt("Flames", flames + 1);
+            return;
         }
+
+        switch (gameObject.tag)
+        {
+            case "Collect":
+                int flames = PlayerPrefs.GetInt("Flames", 0);
+
+                PlayerPrefs.SetInt("Flames", flames + 1);
+
+                break;
+
+            case "Health":
+                int playerHealth = PlayerPrefs.GetInt("Health", 100);
+
+                PlayerPrefs.SetInt("Health", playerHealth + 1);
+                break ;
+        }
+
+        
 
         gameObject.SetActive(false);
     }
