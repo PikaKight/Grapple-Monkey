3e3b8fd56c86a441e9d0c50928b544dc9eb4596f
PikaKight <tmmarcus73@gmail.com>
2025-04-18 20:43:22 -0400

    got it to be more stable

--- Assets/_Scripts/SpikeCollision.cs
+++ Assets/_Scripts/SpikeCollision.cs
@@ -4,7 +4,8 @@ public class SpikeCollision : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D col)
     {
-        collision.collider.gameObject.transform.position = new Vector3(-18, -8, 0);
-        PlayerMovement.canSwing = 0;
+        if (!col.collider.CompareTag("Player")) return;
+        var pm = col.collider.GetComponent<PlayerMovement>();
+        if (pm != null) pm.Respawn();
     }
 }
