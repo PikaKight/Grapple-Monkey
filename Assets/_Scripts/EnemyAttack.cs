c4b39f7f5d861c61a4368d50379bafcd63752092
PikaKight <tmmarcus73@gmail.com>
2025-04-18 21:39:25 -0400

    changing the attack

--- Assets/_Scripts/EnemyAttack.cs
+++ Assets/_Scripts/EnemyAttack.cs
@@ -3,6 +3,9 @@
 [RequireComponent(typeof(EnemyController))]
 public class EnemyAttack : MonoBehaviour
 {
+    [Header("Enemy Type")]
+    public bool hasWeapon = false;
+    
     [Tooltip("Your sword’s trigger collider")]
     public Collider2D hitBox;
 
@@ -12,15 +15,22 @@ public class EnemyAttack : MonoBehaviour
     }
 
     // Animation Event at the moment of impact
-    public void StartAttackHitbox() => hitBox.enabled = true;
-
+    public void StartAttackHitbox()
+    {
+        if (!hasWeapon) return;
+        hitBox.enabled = true;
+    }
     // Animation Event at the last frame of attack
-    public void EndAttackHitbox() => hitBox.enabled = false;
+    public void EndAttackHitbox() { 
+        if (!hasWeapon) return;
+        hitBox.enabled = false;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (!hitBox.enabled) return;
         if (!other.CompareTag("Player")) return;
+
+        if ( hasWeapon && !hitBox.enabled) return;
 
         var pm = other.GetComponent<PlayerMovement>();
         if (pm != null) pm.Respawn();
