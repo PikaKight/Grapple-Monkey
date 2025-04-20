86a90f65ae4a49103f3dade351832e2441773117
PikaKight <tmmarcus73@gmail.com>
2025-04-19 21:07:04 -0400

    finished lvl2

--- Assets/_Scripts/FollowPlayer.cs
+++ Assets/_Scripts/FollowPlayer.cs
@@ -9,6 +9,7 @@ public class FollowPlayer : MonoBehaviour
         // keep camera (or UI) following the player’s X
         if (player == null) return;
         float px = player.position.x;
-        transform.position = new Vector3(Mathf.Max(px, -10f), 0f, -10f);
+        float py = player.position.y;
+        transform.position = new Vector3(Mathf.Max(px, -10f), Mathf.Max(py - 2f, 0f), -10f);
     }
 }
